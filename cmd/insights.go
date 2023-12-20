0949697153042f989544097a4ed9f0ed40f5990d
Steve McDougall <juststevemcd@gmail.com>
2023-12-20 13:25:55 +0000

    Working on styles and sending payload to API

--- cmd/insights.go
+++ cmd/insights.go
@@ -2,26 +2,47 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/treblle/treblle-cli/pkg/http"
+	"github.com/treblle/treblle-cli/pkg/styles"
 )
 
 // insightsCmd is a command to send your OpenAPI Specification to API Insights.
 var insightsCmd = &cobra.Command{
-	Use:   "insights",
+	Use:   "insights [file]",
 	Short: "Run API Insights on your OpenAPI Specification.",
 	Args:  cobra.ExactArgs(1),
 
 	Run: func(cmd *cobra.Command, args []string) {
 		filepath := args[0]
-		mode, _ := cmd.Flags().GetString("mode")
 
-		fmt.Fprintf(cmd.OutOrStdout(), "Processing file: %s with mode: %s\n", filepath, mode)
+		// Check if file exists
+		if _, err := os.Stat(filepath); os.IsNotExist(err) {
+			fmt.Fprintf(cmd.OutOrStderr(), "File does not exist: %s\n", filepath)
+			os.Exit(1)
+		}
+
+		mode, err := cmd.Flags().GetString("mode")
+		if err != nil {
+			fmt.Fprintf(cmd.OutOrStderr(), "Error reading mode flag: %s\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Fprintln(cmd.OutOrStdout(), styles.Info.Render("Processing file: "+filepath+" using mode: "+mode))
+
+		_, err = http.Send("https://httpdump.app/dumps/35be3fed-6393-4a21-89dc-0c4a5ebb59e0", filepath)
+		if err != nil {
+			fmt.Fprintf(cmd.OutOrStderr(), "Request Failed: %s\n\n", err)
+			os.Exit(1)
+		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(insightsCmd)
 
-	insightsCmd.PersistentFlags().String("mode", "CLI", "Mode of operation: CLI (default), IDE, or CI")
+	insightsCmd.Flags().String("mode", "CLI", "Mode of operation (CLI, IDE, or CI)")
 }
---
+++ pkg/styles/styles.go
@@ -0,0 +1,16 @@
+package styles
+
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
+var Text = lipgloss.NewStyle().
+	PaddingLeft(4).
+	PaddingRight(4).
+	PaddingTop(4).
+	PaddingBottom(4)
+
+var Info = Text.
+	Bold(true).
+	Foreground(lipgloss.Color("#FAFAFA")).
+	Background(lipgloss.Color("#7D56F4"))
